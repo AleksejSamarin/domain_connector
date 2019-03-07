a88ba40949d6fa2892054864bf93d1516fcad0f6
Aleksej <samarin113@gmail.com>
2019-03-07 23:29:54 +0300

    comments

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         host, port = sys.argv[1:3] # host = "www.google.com", port = 8080
         try:
-            ip = socket.gethostbyname(host)
+            ip = socket.gethostbyname(host) # получение IP по доменному имени
         except socket.gaierror:
             print(f"Hostname {host} does not exist")
             sys.exit()
         print(f"IP address of {host}: {ip}")
-        socket_desc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket_desc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # настройка сокета
         try:
-            socket_desc.connect((host, int(port)))
+            socket_desc.connect((host, int(port))) # подключение с параметрами
         except KeyboardInterrupt:
             pass
         socket_desc.close()
